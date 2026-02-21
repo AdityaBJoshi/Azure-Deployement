cb80f23f58e2782592c77f17c2710e819c4aa8a7
AdityaBJoshi <imadijoshi@gmail.com>
2026-02-21 09:06:56 +0530

    Azure Springboot App

---
+++ src/main/java/com/azuredemo/controller/MessageController.java
@@ -0,0 +1,18 @@
+package com.azuredemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/msg")
+public class MessageController {
+
+
+    @GetMapping
+    public ResponseEntity<String> getMessage()
+    {
+        return ResponseEntity.ok("Hello....");
+    }
+}
